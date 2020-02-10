7c61f522da393f91e8cb9842066fbdb221061b69
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-10 17:20:33 -0600

    Updates internal functions.

--- R/MatchInternal1.R
+++ R/MatchInternal1.R
@@ -1,5 +1,6 @@
 #' @import matrixStats
-
+#' @useDynLib DSWE
+#' @importFrom Rcpp sourceCpp
 
 CovMatch.Mult = function(dname, cov, wgt, cov.circ){
 
@@ -19,7 +20,7 @@ CovMatch.Mult = function(dname, cov, wgt, cov.circ){
   # Ensuring circular variable to be between 0 to 360 degree
   if(length(cov.circ) > 0) {
 
-    fname_ = lapply(1:length(fname_), function(x) circ.positive(fname_[[x]], cov.circ))
+    fname_ = lapply(1:length(fname_), function(x) Circ.Positive(fname_[[x]], cov.circ))
 
     # Circular variable indicator
     flag = 1
@@ -38,7 +39,7 @@ CovMatch.Mult = function(dname, cov, wgt, cov.circ){
   thres_ = ratio_ * wgt_
 
   # Matching data sets with ref as reference
-  matchID_  = lapply(testid_, function(x) match.cov(ref_, fname_[[x]][, covcol_, drop = F], thres_, cov.circ, flag))
+  matchID_  = lapply(testid_, function(x) Match.Cov(ref_, fname_[[x]][, covcol_, drop = F], thres_, cov.circ, flag))
 
   # creating list of matched data set
   matched_ = rep(list(c()), (length(fname_)))
@@ -73,3 +74,41 @@ CovMatch.Mult = function(dname, cov, wgt, cov.circ){
 
   return(matched_)
 }
+
+
+Match.Cov = function(ref, obj, thres, circ.pos = 0, flag = 0){
+
+  # Ensuring that the data sets are converted to matrix
+  ref = as.matrix(ref)
+  obj = as.matrix(obj)
+
+  match = matchcov(ref = ref , obj = obj, thres = thres, circ_pos = circ.pos, flag = flag)
+
+  # Returns matched index
+  return(match)
+
+}
+
+
+######### Function to convert circular variable values to positive ##################
+# data : data set consiting of features and vlues
+# circ : position of circular variable supplied by user
+Circ.Positive = function(data, circ){
+
+  # Iterating over circular variabe
+  for(i in circ){
+
+    while(sum(data[ , i] < 0) != 0){
+
+      data[, i] = data[, i] + 360
+
+    }
+
+  }
+
+  # Returns manipulated data
+  return(data)
+}
+
+
+
