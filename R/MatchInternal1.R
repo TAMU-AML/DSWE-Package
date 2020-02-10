55e627daa09bec93ab5bb57bab7fad8ad1de0380
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-09 18:10:20 -0600

    Updating covmatch.mult function.

--- R/MatchInternal1.R
+++ R/MatchInternal1.R
@@ -1,67 +1,13 @@
 #' @import matrixStats
 
 
-covmatch.mult = function(dname, cov = NULL, weight, cov_circ = NULL ){
-
-  # Checks for number of data sets provided by user
-  if(length(dname) < 2){
-
-    stop('Atleast two data sets must be provided for matching')
-
-  }
-
-  # Checks whether the provided data set is a list or not
-  if(!is.list(dname)){
-
-    stop('Data set provided should be a list containing data sets')
-
-  }
-
-  # Checks for non circular covariates
-  if(length(cov) > 0){
-
-    if(!is.vector(cov)){
-
-      stop('Non circular covariates column number should be provided as a vector')
-
-    }
-  }
-
-  # Checks for circular covariates
-  if(length(cov_circ) > 0){
-
-    if(!is.vector(cov_circ)){
-
-      stop('Circular covariates column number should be provided as a vector')
-
-    }
-  }
-
-  # Checks whether any covariate is provided by user or not
-  if(!(length(cov) > 0) && !(length(cov_circ) > 0)){
-
-    stop('Atleast a single covariate, either circular or non circular should be provided')
-
-  }
-
-  # Checks for dimension compatibility of weight supplied
-  if(!(length(weight) == length(c(cov, cov_circ)))){
-
-    stop('The weight provided should be a single value or vector with weight for each covariate')
-
-  }
+covmatch.mult = function(dname, weight, cov_circ = NULL ){
 
   # Store data sets to be compared
   f_name = dname
 
   # Weight assigning
   wgt = weight
-
-  # Covariates column number for matching
-  cov_col = c(cov, cov_circ)
-
-  # Circular variable position indicator
-  pos = 0
 
   # Circular variable presence indicator
   flag = 0
@@ -71,9 +17,6 @@ covmatch.mult = function(dname, cov = NULL, weight, cov_circ = NULL ){
 
     f_name = lapply(1:length(f_name), function(x) circ.positive(f_name[[x]], cov_circ))
 
-    # Circular variable after data subsetting position
-    pos = (length(cov)+1):length(cov_col)
-
     # Circular variable indicator
     flag = 1
 
@@ -81,7 +24,7 @@ covmatch.mult = function(dname, cov = NULL, weight, cov_circ = NULL ){
 
   # Setting up the reference data set and threshold
   ref_id = length(f_name)
-  ref = as.matrix(f_name[[ref_id]][, cov_col, drop = F])
+  ref = as.matrix(f_name[[ref_id]])
 
   # Test files
   test_id = c(1:length(f_name))[-ref_id]
@@ -91,7 +34,7 @@ covmatch.mult = function(dname, cov = NULL, weight, cov_circ = NULL ){
   thres = ratio * wgt
 
   # Matching data sets with ref as reference
-  matchID  = lapply(test_id, function(x) match.cov(ref, f_name[[x]][, cov_col, drop = F], thres, circ_pos = pos, flag = flag))
+  matchID  = lapply(test_id, function(x) match.cov(ref, f_name[[x]], thres, circ_pos = cov_circ, flag = flag))
 
   # creating list of matched data set
   matched = rep(list(c()), (length(f_name)))
