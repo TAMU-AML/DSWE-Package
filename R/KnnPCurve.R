5149fc86973947e9ca792c65497917d4c1a03c55
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-14 11:02:32 -0600

    Creates a new script, with main function as KnnPcurve.

---
+++ R/KnnPCurve.R
@@ -0,0 +1,37 @@
+#' @title KNN : PowerCurve
+#' @description The function models the powercurve using KNN, against supplied arguments
+#' @param dname The data set to be used for modelling, could be a dataframe or matrix
+#' @param xvar a vector with column number of non circular features
+#' @param xvar.circ a vector with column number of circular features
+#' @param yvar column number of target
+#' @param kfold split the data set should go through, such as kfold = 10
+#' @param forward_stepwise variable selection technique, which gives the best result for 1 feature, 2 features etc
+#'
+#' @return
+#' @export
+#'
+#' @examples
+KnnPcurve = function(dname, xvar, xvar.circ = NULL,  yvar, kfold, forward_stepwise = FALSE){
+
+  xcol = c(xvar, xvar.circ)
+
+  if(length(xvar.circ) > 0){
+
+    dname[, xvar.circ] = sin(dname[, xvar.circ])
+  }
+
+  for (feature in xcol) {
+
+    dname[, feature] = (dname[, feature] - min(dname[, feature])) / (max(dname[, feature]) - min(dname[, feature]))
+
+  }
+  if(forward_stepwise == FALSE){
+
+    result = KNN.Internal1(dname, xcol, yvar, kfold)
+  }else{
+
+    result = KNN.Internal2(dname, xcol, yvar, kfold)
+  }
+
+  return(result)
+}
