9392ce5b8079176592024f0b4e23cc902e08953b
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-20 15:28:13 -0600

    Documentation added for ComputePCurve function

--- R/ComputePCurve.R
+++ R/ComputePCurve.R
@@ -1,3 +1,17 @@
+#' @title Power curve comparison
+#'
+#' @param data List of data sets to be compared
+#' @param xcol Vector of covariates column number
+#' @param xcol.circ vector of circular covariates column number
+#' @param ycol A value representing column number of target
+#' @param var1col Column number of first covariate to used in generating test set
+#' @param var2col Column number of second covariate to be used in generating test set
+#' @param thrs A single value or vector represnting threshold for each covariates
+#' @param conflevel A single value as a Bound to be used in hypothesis testing while function comparison
+#' @param gridSize A single value to used in constructing test set size
+#'
+#' @export
+
 ComputePCurve = function(data, xcol, xcol.circ, ycol, var1col, var2col, thrs = 0.2, conflevel = 0.95, gridSize = 50){
 
   ResultMatching = CovMatch(data, xcol, xcol.circ, thrs)
