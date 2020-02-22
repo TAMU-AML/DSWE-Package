da328d8f084bcbb41514d45c5a43e1a23d068f3e
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-22 10:02:27 -0600

    ComputePCurve main function body corrected.

--- R/ComputePCurve.R
+++ R/ComputePCurve.R
@@ -22,7 +22,7 @@ ComputePCurve = function(data, xcol, xcol.circ = NULL, ycol, var1col, var2col, t
 
   ResultSMetric = ComputeSMetric(ResultGP$mu1, ResultGP$mu2, ResultGP$band)
 
-  ResultWMetric = ComputeWMetric(data, ResultMatching, ResultGP$mu1, ResultGP$mu2, TestSet, var1col, var2col)
+  ResultWMetric = ComputeWMetric(data, ResultGP$mu1, ResultGP$mu2, TestSet, var1col, var2col)
 
   returnList = list(muDiff = ResultGP$muDiff, mu2 = ResultGP$diffCov$mu2, mu1 = ResultGP$diffCov$mu1, band = ResultGP$band, confLevel = confLevel, testset = testset, estimatedParams = ResultGP$params, weightedDiff = ResultWMetric, statisticalDiff = ResultSMetric)
 
