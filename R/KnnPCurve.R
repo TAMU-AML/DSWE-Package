0c2b13be08f5c32a09bf7e8c9206526455bf0c1c
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-26 18:20:51 -0600

    KnnPCurve documentation updated.

--- R/KnnPCurve.R
+++ R/KnnPCurve.R
@@ -1,17 +1,16 @@
 #' @title KNN : PowerCurve
-#' @description The function models the powercurve using KNN, against supplied arguments
-#' @param dname The data set to be used for modelling, could be a dataframe or matrix
-#' @param xvar a vector with column number of non circular features
-#' @param xvar.circ a vector with column number of circular features
-#' @param yvar column number of target
+#' @description The function models the powercurve using KNN, against supplied arguments.
+#' @param dname The data set to be used for modelling, could be a dataframe or matrix.
+#' @param xvar a vector stating column number of non circular features.
+#' @param xvar.circ a vector stating column number of circular features.
+#' @param yvar a numerical value stating column number of target
 #' @param kfold split the data set should go through, such as kfold = 10
 #' @param forward_stepwise variable selection technique, which gives the best result for 1 feature, 2 features etc
 #'
 #' @return a dataframe consisting of best features and their rmse for forward stepwise, else a numeric value
 #' @export
 #' @import FNN
-#' @examples
-KnnPcurve = function(dname, xvar, xvar.circ = NULL,  yvar, kfold, forward_stepwise = FALSE){
+KnnPCurve = function(dname, xvar, xvar.circ = NULL,  yvar, kfold, forward_stepwise = FALSE){
 
   xcol = c(xvar, xvar.circ)
 
