0a8df7bcbc03173d7136a36be80a4b970d75778b
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-02-27 17:13:47 -0600

    ReductionRatio function call in ComputePCurve function.

--- R/ComputePCurve.R
+++ R/ComputePCurve.R
@@ -24,7 +24,9 @@ ComputePCurve = function(data, xcol, xcol.circ = NULL, ycol, var1col, var2col, t
 
   ResultWMetric = ComputeWMetric(data, ResultGP$mu1, ResultGP$mu2, TestSet, var1col, var2col)
 
-  returnList = list(muDiff = ResultGP$muDiff, mu2 = ResultGP$diffCov$mu2, mu1 = ResultGP$diffCov$mu1, band = ResultGP$band, confLevel = confLevel, testset = testset, estimatedParams = ResultGP$params, weightedDiff = ResultWMetric, statisticalDiff = ResultSMetric)
+  ReductionRatio = ComputeRatio(data, ResultMatching$matchedData, var1col, var2col)
+
+  returnList = list(muDiff = ResultGP$muDiff, mu2 = ResultGP$diffCov$mu2, mu1 = ResultGP$diffCov$mu1, band = ResultGP$band, confLevel = confLevel, testset = testset, estimatedParams = ResultGP$params, weightedDiff = ResultWMetric, statisticalDiff = ResultSMetric, ratioVarcol1 = ReductionRatio$ratioVar1, ratioVarcol2 = ReductionRatio$ratioVar2)
 
   return(returnList)
 }
