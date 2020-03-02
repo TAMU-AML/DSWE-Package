b032ebfda5846897bd748a9749565b269071445b
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-03-02 16:16:44 -0600

    updated KnnSubroutine script.

--- R/KnnSubroutines.R
+++ R/KnnSubroutines.R
@@ -1,65 +1,3 @@
-# Computes knn based rmse for given covariates
-KNN.Internal1 = function(data, xcol, ycol, kfold){
-
-  set.seed(1)
-  data = data[sample(1:nrow(data), nrow(data), replace = FALSE), ]
-  folds = cut(1:nrow(data), breaks = kfold, labels = FALSE)
-  rmse = 0
-
-
-  for(i in 1:kfold)
-  {
-
-    train_data = data[folds != i, ]
-    test_data = data[folds == i, ]
-    folds2 = cut(1:nrow(train_data), breaks = kfold, labels = FALSE)
-
-    best_k = computeBestK(train_data[, xcol], train_data[, ycol], seq(5, 50, 5))
-
-    test_pred = knn.reg(as.matrix(train_data[, xcol]), as.matrix(test_data[, xcol]), train_data[, ycol], k = best_k)
-    residual = sqrt((sum((test_data[, ycol] - test_pred$pred)^2)) / length(test_data[, ycol]))
-    rmse = rmse + residual
-
-  }
-
-  avg_rmse = round(rmse / kfold, 2)
-
-  return(avg_rmse)
-
-}
-
-# Computes knn based forward stepwise : model selection
-KNN.Internal2 = function(data, xcol, ycol,kfold){
-
-  dataframe = data.frame(array(0, dim = c(length(xcol),2)))
-  best.covariate = NULL
-
-  for(itr in 1:(length(xcol))){
-    best.rmse = 1000
-    best.cov = NULL
-    for( covariate in xcol[!xcol %in% best.covariate])
-    {
-      x.var = c(best.covariate, covariate)
-      rmse = KNN.Internal1(data, x.var, ycol, kfold)
-
-      if(rmse < best.rmse){
-
-        best.rmse = rmse
-        best.cov = x.var
-
-      }
-
-    }
-
-    best.covariate = best.cov
-
-    dataframe[itr,1] = paste(best.covariate, collapse="+")
-    dataframe[itr,2] = best.rmse
-
-  }
-  return(dataframe)
-}
-
 # Computes best k using generalized cross validation
 computeBestK = function(dataX, dataY, rangeK ){
 
@@ -69,10 +7,53 @@ computeBestK = function(dataX, dataY, rangeK ){
   nnIdx = knnx.index(dataX, query = dataX, k = maxK)
   gcv = rep(0,length(rangeK))
   for (i in 1:length(rangeK)){
-    predY = rowMeans(matrix(dataY[nnIdx[,1:rangeK[i]]], ncol = ncol(nnIdx[,1:rangeK[i]])))
-    gcv[i] = mean(((dataY-predY)/(1-(1/rangeK[i])))^2)
+    predY = rowMeans(matrix(dataY[nnIdx[, 1:rangeK[i]]], ncol = ncol(nnIdx[, 1:rangeK[i]])))
+    gcv[i] = sqrt(mean(((dataY - predY) / (1 - (1 / rangeK[i])))^2))
   }
   bestK = rangeK[which.min(gcv)]
-  return(bestK)
+  bestRMSE = min(gcv)
+  returnList = list(bestK = bestK, bestRMSE = bestRMSE)
+  if (bestK == maxK){
+    rangeK = maxK + seq(5,50,5)
+    returnList = computeBestK(dataX, dataY, rangeK)
+  }
+  return(returnList)
+
 }
 
+
+# Computes best Subset from given features
+computeBestSubset = function(data, xCol, yCol,rangeK){
+
+  bestSubset = NULL
+  bestRMSE = Inf
+  bestK = NULL
+
+  .computeBestSubset = function(data, xCol, yCol, rangeK, bestSubset, bestRMSE, bestK){
+    nCov = length(xCol)
+    bestCol = NULL
+    for (i in 1:nCov){
+      result = computeBestK(data[, c(bestSubset, xCol[i])], data[, yCol], rangeK)
+      RMSE = result$bestRMSE
+      if (RMSE < bestRMSE){
+        bestRMSE = RMSE
+        bestK = result$bestK
+        bestCol = xCol[i]
+      }
+    }
+
+    returnList = list(bestSubset = bestSubset, bestK = bestK, bestRMSE = bestRMSE )
+
+    if (length(bestCol)>0){
+      bestSubset = c(bestSubset, bestCol)
+      xColDiff = setdiff(xCol, bestSubset)
+      returnList = .computeBestSubset(data, xColDiff, yCol, rangeK, bestSubset, bestRMSE, bestK)
+    }
+
+    return(returnList)
+  }
+
+  returnList = .computeBestSubset(data, xCol, yCol, rangeK, bestSubset, bestRMSE)
+  return(returnList)
+
+}
