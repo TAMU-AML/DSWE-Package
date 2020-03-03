72ec189c1e40bbf531c9614871278cd3cf403757
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-03-02 23:06:18 -0600

    funGP documentation updated.

--- R/funGP.R
+++ R/funGP.R
@@ -1,11 +1,21 @@
 #' @title Function estimator using Gaussian Process
 #'
-#' @param datalist List of data sets
-#' @param covCols Vector of column number of covariates
-#' @param yCol A numeric value for target
-#' @param confLevel Statistical confidence value, such as 0.95
+#' @param datalist a list of data sets to compute a function for each of them
+#' @param covCols a numeric or vector stating the column number of covariates
+#' @param yCol A numeric value stating the column number of target
+#' @param confLevel a statistical value, such as 0.95, to be used for hypothesis testing by computing the band
 #' @param testset Test points at which the functions will be compared
 #'
+#' @return a list containing :
+#'  \itemize{
+#'   \item muDiff - The testset prediction for each of the data set
+#'   \item mu1 - The test prediction for first data set
+#'   \item mu2 - The test prediction for second data set
+#'   \item band - The allowed statistical difference between functions
+#'   \item confLevel - The statistical boundation
+#'   \item testset - The test prediction for first data set
+#'   \item estimatedParams - The function parameter values
+#' }
 #' @export
 funGP = function(datalist, covCols, yCol, confLevel, testset ){
 
