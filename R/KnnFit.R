098c5e9393e933f6b68eb0ebd892516b862de9b2
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-03-02 19:37:40 -0600

    Updated knnFit function.

--- R/KnnFit.R
+++ R/KnnFit.R
@@ -1,36 +1,68 @@
-#' @title KNN : PowerCurve
-#' @description The function models the powercurve using KNN, against supplied arguments.
-#' @param dname The data set to be used for modelling, could be a dataframe or matrix.
-#' @param xvar a vector stating column number of non circular features.
-#' @param xvar.circ a vector stating column number of circular features.
-#' @param yvar a numerical value stating column number of target
-#' @param kfold split the data set should go through, such as kfold = 10
-#' @param forward_stepwise variable selection technique, which gives the best result for 1 feature, 2 features etc
+#' @title KNN : Fit
+#' @description The function models the powercurve using KNN, against supplied arguments
+#' @param data a dataframe or a matrix, to be used in modelling
+#' @param xCol a vector or numeric values stating the column number of features
+#' @param yCol a numerical or a vector value stating the column number of target
+#' @param subsetSelection a boolean, default value is FALSE, if TRUE returns the best beature combination
 #'
-#' @return a dataframe consisting of best features and their rmse for forward stepwise, else a numeric value
+#' @return a list containing :
+#'         1) data
+#'         2) column number of features
+#'         3) column number of target
+#'         4) best K
 #' @export
 #' @import FNN
-KnnPCurve = function(dname, xvar, xvar.circ = NULL,  yvar, kfold, forward_stepwise = FALSE){
+KnnPcurve = function(data, xCol, yCol, subsetSelection = FALSE){
 
-  xcol = c(xvar, xvar.circ)
+  if(!is.matrix(data) || !is.data.frame(data)){
 
-  if(length(xvar.circ) > 0){
-
-    dname[, xvar.circ] = sin(dname[, xvar.circ])
+    stop('data provided should either be a matrix or data frame')
   }
 
-  for (feature in xcol) {
+  if(!is.numeric(xCol)  || !is.vector(xCol)){
 
-    dname[, feature] = (dname[, feature] - min(dname[, feature])) / (max(dname[, feature]) - min(dname[, feature]))
+    stop('column number of features should be provided as a numeric or vector')
 
-  }
-  if(forward_stepwise == FALSE){
-
-    result = KNN.Internal1(dname, xcol, yvar, kfold)
   }else{
 
-    result = KNN.Internal2(dname, xcol, yvar, kfold)
+    if(sum(xCol %in% 1:ncol(data)) < length(xCol)){
+
+      stop('column numbers provided are not in the data')
+    }
   }
 
-  return(result)
+  if(!is.numeric(yCol)  || !is.vector(yCol)){
+
+    stop('column number of target should be provided as a numeric or vector')
+
+  }else{
+
+    if(length(yCol) > 1){
+
+      stop('a signle numeric or vector input should be provided for target')
+    }
+  }
+
+  normalizedData = data
+
+  for (feature in xCol) {
+
+    normalizedData[, feature] = (data[, feature] - min(data[, feature])) / (max(data[, feature]) - min(data[, feature]))
+
+  }
+
+  rangeK = seq(5,50,5)
+
+  if(subsetSelection == FALSE){
+
+    result = computeBestK(normalizedData[, xCol], normalizedData[, yCol], rangeK)
+    returnList = list(bestK = result$bestK, bestRMSE = result$bestRMSE, data = data, xCol = xCol, yCol = yCol)
+
+  }else{
+
+    result = computeBestSubset(normalizedData, xCol, yCol, rangeK)
+    returnList = list(bestK = result$bestK, bestRMSE = result$bestRMSE, data = data, xCol = result$bestSubset, yCol = yCol )
+  }
+
+  return(returnList)
 }
