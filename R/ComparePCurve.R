d93175a810f5fcd1826bb8196bbe8309337299ad
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-03-17 00:31:31 -0500

    ComparePCurve updated

--- R/ComparePCurve.R
+++ R/ComparePCurve.R
@@ -81,7 +81,7 @@ ComparePCurve = function(data, xCol, xCol.circ = NULL, yCol, testCol, testSet = 
 
   if(is.null(testSet)){
 
-  testSet = GeneratetestSet(resultMatching$matchedData, testCol, gridSize )
+  testSet = GenerateTestSet(resultMatching$matchedData, testCol, gridSize )
 
         if(!is.vector(gridSize)){
 
