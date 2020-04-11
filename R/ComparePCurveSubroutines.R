63d81a5820f7225e1dfb8420cd6f154357850e17
Abhinav Prakash <abhinavp@tamu.edu>
2020-04-11 15:22:18 -0500

    Added functions to compute unweighted difference

--- R/ComparePCurveSubroutines.R
+++ R/ComparePCurveSubroutines.R
@@ -13,6 +13,48 @@ GenerateTestset = function(data, testCol, gridSize){
 
 }
 
+# Compute unweigted difference between the functions
+ComputeDiff = function(mu1, mu2){
+
+  diff = sum(mu1 - mu2)/length(mu1)
+  avgMu = mean(mu1 + mu2)/2
+  percentDiff = diff*100/avgMu
+  return(percentDiff)
+
+}
+
+# Compute statistically significant unweigted difference between the functions
+ComputedStatDiff = function(mu1, mu2, band){
+
+  diffMu = sum(mu1 - mu2)
+  diffMu[which(abs(Diff)<band)] = 0
+  diff = sum(diffMu)/lenght(mu1)
+  avgMu = mean(mu1 + mu2)/2
+  percentDiff = diff*100/avgMu
+  return(percentDiff)
+
+}
+
+
+# Compute weighted metrics
+ComputeWeightedDiff = function(dList, mu1, mu2, testdata, testCol){
+
+  mixedData = rbind(dList[[1]], dList[[2]])
+
+  var1Density = density(mixedData[, testCol[1]])
+  var1Test = approx(var1Density$x, var1Density$y, xout = testdata[, 1])
+
+  var2Density = density(mixedData[, testCol[2]])
+  var2Test = approx(var2Density$x, var2Density$y, xout = testdata[, 2])
+
+  probTest = var1Test$y * var2Test$y / (sum(var1Test$y * var2Test$y))
+
+  resultP = round((sum((mu1 - mu2) * (probTest)) / (sum((mu1 + mu2)* (probTest)) / 2)) * 100, 2)
+  resultA = sum(((mu1 - mu2)) * (probTest))
+
+  return(resultP)
+
+}
 
 # Compute statistical weighted metrics
 ComputeWeightedStatDiff = function(dList, mu1, mu2, band, testdata, testCol){
@@ -36,7 +78,61 @@ ComputeWeightedStatDiff = function(dList, mu1, mu2, band, testdata, testCol){
 
 }
 
-# Compute weighted statistical extrapolatiion
+# Compute difference in the function scaled to the orginal data
+ComputeScaledDiff = function(datalist, yCol, mu1, mu2){
+  mergedData = rbind(datalist[[1]],datalist[[2]])
+  pw = mergedData[,yCol]
+  pw[pw < 0] = 0
+  binWidth = 100
+
+  #starting bin value
+  start = 0
+
+  #ending value based on max power rounded up to the closest multiple of the bin width.
+  #This formulation ensures that the last bin of the original data will never be empty.
+  end = max(pw) + (binWidth - (max(pw)%%binWidth))
+  bins = seq(start,end,binWidth)
+
+  #Merge the empty bins till there are no empty bins in any of the three: pw, mu1, mu2
+  cumCount = sapply(c(1:length(bins)), function (x) length(which(pw < bins[x]))) #cumulative count for each bin
+  nonEmptyBins = bins[!duplicated(cumCount)] #remove bins with cumulative count same as the previous bin i.e. remove empty bins
+
+  #Repeat the above two lines for mu1 and mu2
+  cumCount = sapply(c(1:length(nonEmptyBins)), function (x) length(which(mu1 < nonEmptyBins[x])))
+  nonEmptyBins = nonEmptyBins[!duplicated(cumCount)]
+  cumCount = sapply(c(1:length(nonEmptyBins)), function (x) length(which(mu2 < nonEmptyBins[x])))
+  nonEmptyBins = nonEmptyBins[!duplicated(cumCount)]
+
+  #Check if the last bin is equal to 'end'. If not, replace last bin value with 'end'
+  if (max(nonEmptyBins)<end){
+    nonEmptyBins[length(nonEmptyBins)] = end
+  }
+
+  #Compute probability for each merged bin
+  totalCount = length(pw)
+  probVector = sapply(c(2:length(nonEmptyBins)), function (x) length(which(pw >= nonEmptyBins[x-1] & pw < nonEmptyBins[x]))/totalCount)
+
+  #pointwise delta and average mu
+  delta = mu1 - mu2
+  mu = 0.5*(mu1+mu2)
+
+  #bin wise average delta and mu
+  avgDelta1 = sapply(c(2:length(nonEmptyBins)), function (x) mean(delta[(which(mu1 >= nonEmptyBins[x-1] & mu1 < nonEmptyBins[x]))]))
+  avgMu1 = sapply(c(2:length(nonEmptyBins)), function (x) mean(mu[(which(mu1 >= nonEmptyBins[x-1] & mu1 < nonEmptyBins[x]))]))
+  avgDelta2 = sapply(c(2:length(nonEmptyBins)), function (x) mean(delta[(which(mu2 >= nonEmptyBins[x-1] & mu2 < nonEmptyBins[x]))]))
+  avgMu2 = sapply(c(2:length(nonEmptyBins)), function (x) mean(mu[(which(mu2 >= nonEmptyBins[x-1] & mu2 < nonEmptyBins[x]))]))
+
+  deltaBin = 0.5*(avgDelta1+avgDelta2)
+  muBin = 0.5*(avgMu1+avgMu2)
+
+  scaledDiff = t(probVector)%*%deltaBin
+  percentScaledDiff = scaledDiff*100/(t(probVector)%*%muBin)
+
+  return(as.numeric(percentScaledDiff))
+}
+
+
+# Compute statistically significant difference in the function scaled to the orginal data
 ComputeScaledStatDiff = function(datalist, yCol, mu1, mu2, band){
   mergedData = rbind(datalist[[1]],datalist[[2]])
   pw = mergedData[,yCol]
@@ -90,79 +186,6 @@ ComputeScaledStatDiff = function(datalist, yCol, mu1, mu2, band){
   return(as.numeric(percentScaledDiff))
 }
 
-
-# Compute weighted metrics
-ComputeWeightedDiff = function(dList, mu1, mu2, testdata, testCol){
-
-  mixedData = rbind(dList[[1]], dList[[2]])
-
-  var1Density = density(mixedData[, testCol[1]])
-  var1Test = approx(var1Density$x, var1Density$y, xout = testdata[, 1])
-
-  var2Density = density(mixedData[, testCol[2]])
-  var2Test = approx(var2Density$x, var2Density$y, xout = testdata[, 2])
-
-  probTest = var1Test$y * var2Test$y / (sum(var1Test$y * var2Test$y))
-
-  resultP = round((sum((mu1 - mu2) * (probTest)) / (sum((mu1 + mu2)* (probTest)) / 2)) * 100, 2)
-  resultA = sum(((mu1 - mu2)) * (probTest))
-
-  return(resultP)
-
-}
-
-# Compute weighted extrapolatiion
-ComputeScaledDiff = function(mergedData, yCol, mu1, mu2){
-  mergedData = rbind(datalist[[1]],datalist[[2]])
-  pw = mergedData[,yCol]
-  pw[pw < 0] = 0
-  binWidth = 100
-
-  #starting bin value
-  start = 0
-
-  #ending value based on max power rounded up to the closest multiple of the bin width.
-  #This formulation ensures that the last bin of the original data will never be empty.
-  end = max(pw) + (binWidth - (max(pw)%%binWidth))
-  bins = seq(start,end,binWidth)
-
-  #Merge the empty bins till there are no empty bins in any of the three: pw, mu1, mu2
-  cumCount = sapply(c(1:length(bins)), function (x) length(which(pw < bins[x]))) #cumulative count for each bin
-  nonEmptyBins = bins[!duplicated(cumCount)] #remove bins with cumulative count same as the previous bin i.e. remove empty bins
-
-  #Repeat the above two lines for mu1 and mu2
-  cumCount = sapply(c(1:length(nonEmptyBins)), function (x) length(which(mu1 < nonEmptyBins[x])))
-  nonEmptyBins = nonEmptyBins[!duplicated(cumCount)]
-  cumCount = sapply(c(1:length(nonEmptyBins)), function (x) length(which(mu2 < nonEmptyBins[x])))
-  nonEmptyBins = nonEmptyBins[!duplicated(cumCount)]
-
-  #Check if the last bin is equal to 'end'. If not, replace last bin value with 'end'
-  if (max(nonEmptyBins)<end){
-    nonEmptyBins[length(nonEmptyBins)] = end
-  }
-
-  #Compute probability for each merged bin
-  totalCount = length(pw)
-  probVector = sapply(c(2:length(nonEmptyBins)), function (x) length(which(pw >= nonEmptyBins[x-1] & pw < nonEmptyBins[x]))/totalCount)
-
-  #pointwise delta and average mu
-  delta = mu1 - mu2
-  mu = 0.5*(mu1+mu2)
-
-  #bin wise average delta and mu
-  avgDelta1 = sapply(c(2:length(nonEmptyBins)), function (x) mean(delta[(which(mu1 >= nonEmptyBins[x-1] & mu1 < nonEmptyBins[x]))]))
-  avgMu1 = sapply(c(2:length(nonEmptyBins)), function (x) mean(mu[(which(mu1 >= nonEmptyBins[x-1] & mu1 < nonEmptyBins[x]))]))
-  avgDelta2 = sapply(c(2:length(nonEmptyBins)), function (x) mean(delta[(which(mu2 >= nonEmptyBins[x-1] & mu2 < nonEmptyBins[x]))]))
-  avgMu2 = sapply(c(2:length(nonEmptyBins)), function (x) mean(mu[(which(mu2 >= nonEmptyBins[x-1] & mu2 < nonEmptyBins[x]))]))
-
-  deltaBin = 0.5*(avgDelta1+avgDelta2)
-  muBin = 0.5*(avgMu1+avgMu2)
-
-  scaledDiff = t(probVector)%*%deltaBin
-  percentScaledDiff = scaledDiff*100/(t(probVector)%*%muBin)
-
-  return(as.numeric(percentScaledDiff))
-}
 
 # Compute reduction ratio
 ComputeRatio = function(dataList1, dataList2, testCol){
