222d2be57e8c7c8b735cf80a378028f61c782c76
Abhinav Prakash <abhinavp@tamu.edu>
2020-05-06 21:53:00 -0500

    Removed the extra argument added mistakenly to ComputeRatio

--- R/ComparePCurve.R
+++ R/ComparePCurve.R
@@ -157,7 +157,7 @@ ComparePCurve = function(data, xCol, xCol.circ = NULL, yCol, testCol, testSet = 
 
   unweightedStatDiff = ComputeStatDiff(resultGP$mu1, resultGP$mu2, resultGP$band, baseline)
 
-  reductionRatio = ComputeRatio(data, resultMatching$matchedData, testCol, baseline)
+  reductionRatio = ComputeRatio(data, resultMatching$matchedData, testCol)
 
   returnList = list(weightedDiff = weightedDiff, weightedStatDiff = weightedStatDiff, scaledDiff = scaledDiff, scaledStatDiff = scaledStatDiff, unweightedDiff = unweightedDiff, unweightedStatDiff = unweightedStatDiff, reductionRatio = reductionRatio, muDiff = resultGP$muDiff, mu2 = resultGP$mu2, mu1 = resultGP$mu1, band = resultGP$band, confLevel = conflevel, testSet = testSet, estimatedParams = resultGP$estimatedParams)
 
