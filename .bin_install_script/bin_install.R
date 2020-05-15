233eea5fb03d6f0dfe1253eaf84c443e89a4e1e7
Abhinav Prakash <abhinavp@tamu.edu>
2020-05-15 02:54:56 -0500

    bin_install.R: Making the variables hidden and cleaning the environment after install

--- .bin_install_script/bin_install.R
+++ .bin_install_script/bin_install.R
@@ -3,8 +3,8 @@
 #DESCRIPTION: This script can be used to install DSWE package from its binary file
 ##################################################################################
 
-PACKAGE_VERSION = "1.3.1" #Update this line when version changes
-DEPENDENCIES = list(
+.PACKAGE_VERSION = "1.3.1" #Update this line when version changes
+.DEPENDENCIES = list(
   list(name = "Rcpp",version = "1.0.4.6"),
   list(name = "matrixStats",version = "0.55.0"),
   list(name = "FNN",version = "1.1.3"),
@@ -13,22 +13,24 @@ DEPENDENCIES = list(
 )
 
 if (Sys.info()["sysname"]=="Windows"){
-  FILENAME = paste0("https://github.com/TAMU-AML/DSWE-Package/releases/download/v",PACKAGE_VERSION,"/DSWE_",PACKAGE_VERSION,".zip")  
+  .FILENAME = paste0("https://github.com/TAMU-AML/DSWE-Package/releases/download/v",.PACKAGE_VERSION,"/DSWE_",.PACKAGE_VERSION,".zip")  
 } else {
-  FILENAME = paste0("https://github.com/TAMU-AML/DSWE-Package/releases/download/v",PACKAGE_VERSION,"/DSWE_",PACKAGE_VERSION,".tgz")  
+  .FILENAME = paste0("https://github.com/TAMU-AML/DSWE-Package/releases/download/v",.PACKAGE_VERSION,"/DSWE_",.PACKAGE_VERSION,".tgz")  
 }
 
-packageList = utils::installed.packages()[,1]
+.packageList = utils::installed.packages()[,1]
 
-for (i in c(1:length(DEPENDENCIES))){
-  if (DEPENDENCIES[[i]]$name %in% packageList){
-    available_version = utils::packageVersion(DEPENDENCIES[[i]]$name)
-    if (available_version < DEPENDENCIES[[i]]$version){
-      update.packages(DEPENDENCIES[[i]]$name)
+for (.i in c(1:length(.DEPENDENCIES))){
+  if (.DEPENDENCIES[[.i]]$name %in% .packageList){
+    .available_version = utils::packageVersion(.DEPENDENCIES[[.i]]$name)
+    if (.available_version < .DEPENDENCIES[[.i]]$version){
+      update.packages(.DEPENDENCIES[[.i]]$name)
     }
   } else {
-    utils::install.packages(DEPENDENCIES[[i]]$name)
+    utils::install.packages(.DEPENDENCIES[[.i]]$name)
   }
 }
 
-utils::install.packages(FILENAME,repos=NULL)
+utils::install.packages(.FILENAME,repos=NULL)
+
+rm(list = c(".PACKAGE_VERSION",".DEPENDENCIES",".FILENAME",".packageList",".i"))
