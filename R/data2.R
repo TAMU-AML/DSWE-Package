33101e471124b8ea9dce308e43a4a35235be60c3
Nitesh Kumar <nitesh.kumar@tamu.edu>
2020-07-27 01:50:45 -0500

    data set documentation added.

---
+++ R/data2.R
@@ -0,0 +1,22 @@
+#' Wind Energy data set containing 48,068 data points
+#'
+#' A dataset containing the power produced and other attributes of almost 48,068
+#' records.
+#'
+#' \itemize{
+#'   \item Data.point - sequence of integers displaying each record
+#'   \item V - wind speed
+#'   \item D - wind direction
+#'   \item air.density - air density
+#'   \item I - intensity
+#'   \item S_b - standard deviation
+#'   \item Y - target or power produced
+
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name data2
+#' @usage data(data2)
+#' @format A data frame with 48,068 rows and 7 variables
+NULL
